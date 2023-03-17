fd181ca5c21e13b44d608f02d6446ec8016b7935
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+Nurkrao2002@users.noreply.github.com>
2023-03-18 00:52:37 +0530

    Add files via upload

---
+++ DB-ORIGINAL/index.js
@@ -0,0 +1,66 @@
+var pool = require("./connection")
+var express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+var bodyparser = require("body-parser");
+
+app.use(bodyparser.json());
+
+app.use(bodyparser.urlencoded({ extended:true}));
+
+app.set("view engine","ejs");
+
+// app.use(express.static(__dirname + '/students.ejs'));
+
+
+// app.get("/", function(req, res){
+//     res.sendFile(__dirname +"/register.html");
+
+// });
+
+    // to connect database or else to display not connected database...
+
+pool.connect((err, client, done) => {
+  if (err) {
+    console.error('', err);
+    // Redirect to another page
+    app.get('/', (req, res) => {
+      res.redirect('/error');
+      
+    });
+  } else {
+    console.log('Connected to database');
+    // Continue with your application logic
+    app.get('/', (req, res) => {
+    //   res.render(__dirname +"/search-students.ejs");
+    // res.send('database error. Please try again later.....!');
+    return res.redirect('/home');
+    });
+  }
+});
+
+app.get('/error', (req, res) => {
+//   res.send('database error. Please try again later.....!');
+//   res.render(__dirname +"/error.ejs");
+  // return res.redirect('/error');
+  res.render(__dirname +"/error.ejs");
+});
+
+
+app.get('/home', (req, res) => {
+  res.render(__dirname+"/index.ejs");
+  // res.send('/index.ejs')
+})
+
+
+
+
+
+
+
+// ---------------------------------------------------------------------------------------------------
+
+app.listen(7000, function(){
+    console.log("Sever Is Connected....at port no - 7000!")
+});
